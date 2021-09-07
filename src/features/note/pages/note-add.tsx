360bbfc8a9d6004ee601c13803803c9e95f24fdc
Danayit Mulugeta <dayni0603@gmail.com>
2021-09-07 16:20:15 +0300

    init project setup fix

---
+++ src/features/note/pages/note-add.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoteAdd() {
+  return <div>NoteAdd</div>;
+}
+
+export default NoteAdd;
---
+++ src/features/note/pages/note-update.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function NoteUpdate() {
+  return <div>NoteUpdate</div>;
+}
+
+export default NoteUpdate;
